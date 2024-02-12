9681bfeba9db3caa7eccec54793205cdc303d6fd
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2024-02-12 16:43:03 +0900

    beakjon 10809

---
+++ src/string/BJ10809.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BJ10809 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] alpaArr = new int[26];
+        Arrays.fill(alpaArr, -1);
+
+        String s = br.readLine();
+        for ( int i = 0 ; i <s.length();i++) {
+            int a = s.charAt(i)-97;
+            if ( alpaArr[a] == -1) {
+                alpaArr[a] = i;
+            }
+        }
+        for ( int a : alpaArr){
+            bw.write(a +" ");
+        }
+        bw.flush();
+    }
+}
